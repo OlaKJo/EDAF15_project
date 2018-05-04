c7b8d3a30fc64e490eef1270bcfe61c4a0e4bf6a
Joachim Wedin <nat11jwe@student.lu.se>
2018-05-04 11:43:09 +0200

    Lots of refactoring done. Added group_sort and swap_row. Removed copy_row. Todo: integrate a and c.

--- small/Test_small.c
+++ small/Test_small.c
@@ -8,8 +8,6 @@ void test_addq_1(void)
     rational res = addq(r1, r2);
 
     rational expected_res = {.p = 6, .q = 1};
-    print_rational(res);
-    print_rational(expected_res);
     TEST_ASSERT(res.p == expected_res.p);
     TEST_ASSERT(res.q == expected_res.q);
 }
@@ -21,8 +19,6 @@ void test_addq_2(void)
     rational res = addq(r1, r2);
 
     rational expected_res = {.p = 2, .q = 1};
-    print_rational(res);
-    print_rational(expected_res);
     TEST_ASSERT(res.p == expected_res.p);
     TEST_ASSERT(res.q == expected_res.q);
 }
@@ -30,11 +26,9 @@ void test_addq_2(void)
 void test_reduce_zero(void)
 {
     rational r1 = {.p = 0, .q = 1};
-    reduce(&r1);
+    r1 = reduce(r1);
 
     rational expected_res = {.p = 0, .q = 1};
-    print_rational(r1);
-    print_rational(expected_res);
     TEST_ASSERT(r1.p == expected_res.p);
     TEST_ASSERT(r1.q == expected_res.q);
 }
@@ -45,13 +39,8 @@ void test_sign(void)
     rational r2 = {.p = -3, .q = 1};
     rational r3 = {.p = 0, .q = 3};
 
-    print_rational(r1);
     TEST_ASSERT(sign(r1) == 1);
-    
-    print_rational(r2);
     TEST_ASSERT(sign(r2) == -1);
-
-    print_rational(r3);
     TEST_ASSERT(sign(r3) == 0);
 }
 
@@ -61,7 +50,6 @@ void test_div_zero(void)
     rational r2 = {.p = 0, .q = 1};
 
     rational res = divq(r2, r1);
-    print_rational(res);
 
     rational expected_res = {.p = 0, .q = 1};
     TEST_ASSERT(res.p == expected_res.p);
@@ -78,6 +66,30 @@ void test_big_div(void)
     TEST_ASSERT(res.p == sol.p);
     TEST_ASSERT(res.q == sol.q);
 }
+
+test_group_sort()
+{
+    rational r1 = {.p = 0, .q = 1};
+    rational r2 = {.p = 1, .q = 1};
+    rational r3 = {.p = -2, .q = 1};
+    rational r4 = {.p = 3, .q = 1};
+    rational r5 = {.p = -4, .q = 1};
+    rational r6 = {.p = 0, .q = 1};
+    rational r7 = {.p = 5, .q = 1};
+
+    rational a[3][1] = {r1, r2, r3};
+    rational c[3] = {r4, r5, r6};
+
+    group_sort(3, 1, a, c);
+    
+    print_rational(a[0][0]);
+    print_rational(a[1][0]);
+    print_rational(a[2][0]);
+    TEST_ASSERT(compare(a[0][0], r2) == 0);
+    TEST_ASSERT(compare(a[1][0], r3) == 0);
+    TEST_ASSERT(compare(a[2][0], r1) == 0);
+}
+
 void test_gcd_zero(void)
 {
     TEST_ASSERT(gcd(0, 2) == 1);
@@ -91,5 +103,6 @@ int main(void)
     RUN_TEST(test_sign);
     RUN_TEST(test_div_zero);
     RUN_TEST(test_big_div);
+    RUN_TEST(test_group_sort);
     return UNITY_END();
 }
--- small/small.c
+++ small/small.c
@@ -11,43 +11,29 @@ typedef struct
 
 bool fm_rat(size_t rows, size_t cols, rational a[rows][cols], rational c[rows]);
 
-long abs_long(long l)
-{
-	if (l < 0) 
-		return l * (-1);
-	return l;
-}
 long gcd(long a, long b)
 {
-	long tmp;
 	long rem;
 
 	if (a < b)
-	{
-		tmp = a;
-		a = b;
-		b = tmp;
-	}
+		return gcd(b,a);
 
-	rem = a % b;
-
-	while (rem != 0)
+	while ((rem = a % b) != 0)
 	{
 		a = b;
 		b = rem;
-		rem = a % b;
 	}
 
-	return abs_long(b);
+	return labs(b);
 }
 
 rational reduce(rational r)
 {
+	int g;
 	if (r.p == 0)
 		return r;
-	int g;
 	while ((g = gcd(r.p, r.q)) != 1)
-	{	
+	{
 		r.p /= g;
 		r.q /= g;
 	}
@@ -56,7 +42,7 @@ rational reduce(rational r)
 
 rational new_rational(long p, long q)
 {
- 	return (rational) {.p = (q < 0 ? p * -1 : p), .q = (q < 0 ? q* -1 : q)};
+	return (rational){.p = (q < 0 ? p * -1 : p), .q = (q < 0 ? q * -1 : q)};
 }
 
 rational addq(rational f1, rational f2)
@@ -80,7 +66,7 @@ rational divq(rational f1, rational f2)
 	return mulq(f1, new_rational(f2.q, f2.p));
 }
 
-int sign(rational r)
+signed char sign(rational r)
 {
 	return (r.p < 0 ? -1 : r.p == 0 ? 0 : 1);
 }
@@ -92,7 +78,6 @@ int compare(rational r1, rational r2)
 
 bool fm(size_t rows, size_t cols, signed char a[rows][cols], signed char c[rows])
 {
-
 	rational new_a[rows][cols];
 	rational new_c[rows];
 
@@ -108,66 +93,90 @@ bool fm(size_t rows, size_t cols, signed char a[rows][cols], signed char c[rows]
 	return fm_rat(rows, cols, new_a, new_c);
 }
 
-void copyRow(size_t rows, size_t cols, rational a[rows][cols], int src_pos, rational b[rows][cols], int dest_pos)
+void swap_rows(size_t rows, size_t cols, rational a[rows][cols], int index1, int index2)
 {
+	rational tmp;
 	for (size_t i = 0; i < cols; i++)
 	{
-		b[dest_pos][i] = a[src_pos][i];
+		tmp = a[index1][i];
+		a[index1][i] = a[index2][i];
+		a[index2][i] = tmp;
+	}
+}
+
+//void group_sort(size_t rows, size_t cols, rational a[rows][cols], rational c[rows])
+//{
+//	int pos = 0;
+//	int zeros = rows - 1;
+//	rational * temp;
+//	rational c_temp;
+//	for (size_t j = 0; j < rows; j++) {
+//		signed char s = sign(a[j][cols - 1]);
+//		if (s > 0) {
+//			temp = a[pos];
+//			a[pos] = ((rational *) (a + j));
+//			*(a + j) = temp;
+//			c_temp = c[pos];
+//			c[pos++] = c[j];
+//			c[j] = c_temp;
+//		} else if (s == 0) {
+//			temp = a[zeros];
+//			a[zeros--] = a[j];
+//			a[j] = temp;
+//			c_temp = c[zeros];
+//			c[zeros] = c[j];
+//			c[j] = c_temp;
+//			j--;
+//		}
+//}
+
+void group_sort(size_t rows, size_t cols, rational a[rows][cols], rational c[rows], int *n1, int *n2)
+{
+	int pos = 0;
+	int zeros = rows - 1;
+	rational *temp;
+	rational c_temp;
+	for (size_t j = 0; j <= zeros; j++)
+	{
+		signed char s = sign(a[j][cols - 1]);
+		if (s > 0)
+		{
+			(*n1)++;
+			swap_rows(rows, cols, a, j, pos);
+			swap_rows(rows, 1, c, j, pos++);
+		}
+		else if (s == 0)
+		{
+			(*n2)--;
+			swap_rows(rows, cols, a, j, zeros);
+			swap_rows(rows, 1, c, j, zeros--);
+			j--;
+		}
 	}
 }
 
 bool fm_rat(size_t rows, size_t cols, rational a[rows][cols], rational c[rows])
 {
-	rational new_a[rows][cols];
-	rational new_c[rows];
-
-	int order[3] = {1, -1, 0};
-
 	// step 1
 	int r = cols;
 	int s = rows;
 
 	// step 2
 	int n1 = 0;
-	int n2 = 0;
-	int currentIndex = 0;
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < s; j++)
-		{
-			if (order[i] * sign(a[j][r - 1]) > 0)
-			{
-				copyRow(rows, cols, a, j, new_a, currentIndex);
+	int n2 = rows;
 
-				new_c[currentIndex++] = c[j];
-				if (order[i] == 1)
-					n1++;
-				else
-					n2++;
-								
-
-				//printf("%i\n", new_c[currentIndex - 1]);
-			}
-			else if (a[j][r - 1].p == 0 && order[i] == 0)
-			{
-				copyRow(rows, cols, a, j, new_a, currentIndex);
-				new_c[currentIndex++] = c[j];
-			}
-		}
-	}
-
-	n2 += n1;
+	group_sort(rows, cols, a, c, &n1, &n2);
 
 	// step 3
 	for (int i = 0; i < n2; i++)
 	{
 		for (int j = 0; j < r - 1; j++)
 		{
-			new_a[i][j] = divq(new_a[i][j], new_a[i][r - 1]);
+			a[i][j] = divq(a[i][j], a[i][r - 1]);
 		}
-		new_c[i] = divq(new_c[i], new_a[i][r - 1]);
+		c[i] = divq(c[i], a[i][r - 1]);
 	}
-	
+
 	// more variables needs to be eliminated
 	if (r > 1)
 	{
@@ -188,10 +197,9 @@ bool fm_rat(size_t rows, size_t cols, rational a[rows][cols], rational c[rows])
 				for (int l = 0; l < r - 1; l++)
 				{
 					rational rat = {.p = -1, .q = 1};
-					a_prim[currentRow][l] = addq(mulq(new_a[j][l], rat), new_a[i][l]);
-
+					a_prim[currentRow][l] = addq(mulq(a[j][l], rat), a[i][l]);
 				}
-				c_prim[currentRow] = subq(new_c[i], new_c[j]);
+				c_prim[currentRow] = subq(c[i], c[j]);
 				currentRow++;
 			}
 		}
@@ -200,9 +208,9 @@ bool fm_rat(size_t rows, size_t cols, rational a[rows][cols], rational c[rows])
 		{
 			for (int j = 0; j < r - 1; j++)
 			{
-				a_prim[currentRow][j] = new_a[i][j];
+				a_prim[currentRow][j] = a[i][j];
 			}
-			c_prim[currentRow] = new_c[i];
+			c_prim[currentRow] = c[i];
 			currentRow++;
 		}
 
@@ -214,27 +222,27 @@ bool fm_rat(size_t rows, size_t cols, rational a[rows][cols], rational c[rows])
 		if ((s - n2 + n1 * (n2 - n1)) == 0)
 			return true;
 		//step 5
-		rational max = new_c[n1];
-		rational min = new_c[0];
+		rational max = c[n1];
+		rational min = c[0];
 		for (int i = 1; i < n1; i++)
 		{
-			if (compare(new_c[i], min) == -1)
+			if (compare(c[i], min) == -1)
 			{
-				min = new_c[i];
+				min = c[i];
 			}
 		}
 
 		for (int i = n1 + 1; i < n2; i++)
 		{
-			if (compare(new_c[i], max) == 1)
+			if (compare(c[i], max) == 1)
 			{
-				max = new_c[i];
+				max = c[i];
 			}
 		}
 
 		for (int i = n2; i < s; i++)
 		{
-			if (sign(new_c[i]) == -1)
+			if (sign(c[i]) == -1)
 				return false;
 		}
 
